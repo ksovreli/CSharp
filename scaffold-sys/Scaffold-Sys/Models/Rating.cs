6dbf3ac006e0e348849419a2ded232064b6346b2
ksovreli <sandroqsovreli@gmail.com>
2026-01-24 21:53:58 +0400

    Add restored scaffold-sys project

---
+++ scaffold-sys/Scaffold-Sys/Models/Rating.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SKA_Holding.Models;
+
+[Table("Rating")]
+[Index("UserId", "MovieId", Name = "UQ__Rating__A335E50C3E2662FC", IsUnique = true)]
+public partial class Rating
+{
+    [Key]
+    public int RatingId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int MovieId { get; set; }
+
+    public int? Score { get; set; }
+
+    public DateOnly? RatedAt { get; set; }
+
+    [ForeignKey("MovieId")]
+    [InverseProperty("Ratings")]
+    public virtual Movie Movie { get; set; } = null!;
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Ratings")]
+    public virtual User User { get; set; } = null!;
+}
---
+++ scaffold-sys/Scaffold-Sys/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SKA_Holding.Models;
+
+[Table("User")]
+[Index("Username", Name = "UQ__User__536C85E40DD33224", IsUnique = true)]
+[Index("Email", Name = "UQ__User__A9D10534A0747089", IsUnique = true)]
+public partial class User
+{
+    [Key]
+    public int UserId { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Username { get; set; } = null!;
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string? Email { get; set; }
+
+    [StringLength(50)]
+    public string? FirstName { get; set; }
+
+    [StringLength(50)]
+    public string? LastName { get; set; }
+
+    public DateOnly? BirthDate { get; set; }
+
+    [InverseProperty("User")]
+    public virtual ICollection<Rating> Ratings { get; set; } = new List<Rating>();
+
+    [InverseProperty("User")]
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
